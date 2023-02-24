f6d9129a2d3c855537e8397d983e503e99600bbc
Felipe27200 <felipe3841@gmail.com>
2023-02-23 20:49:42 -0500

    Hola mundo basic with spring MVC

--- src/main/java/com/Springboottutorial/controller/GreetingController.java
+++ src/main/java/com/Springboottutorial/controller/GreetingController.java
@@ -63,6 +63,9 @@ public class GreetingController {
 		 * */
 		model.addAttribute("name", name);
 		
-		return "gretting";
+		/*
+		 * Debe coincidir con el nombre de la ruta
+		 * */
+		return "greeting";
 	}
 }
